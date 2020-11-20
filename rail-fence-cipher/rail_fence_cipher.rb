8082555fd9e69f2c002918d5fa70d383f98dfcba
Pascal P <lacsap_666@yahoo.fr>
2020-11-21 10:36:25 +1300

    rail-fence-cipher: refactoring + comments

--- rail-fence-cipher/rail_fence_cipher.rb
+++ rail-fence-cipher/rail_fence_cipher.rb
@@ -20,19 +20,22 @@ class RailFenceCipher
 
   def encode()
     @rails == 1 && (return @msg)
-    r = fill(@msg)
-    0.upto(@rails - 1).
+    r = fill(@msg)               ## fill the rails
+
+    ## now, encode
+    enum_rails.
       inject([]) {|a, jx| a << r[jx].select {|ch| ch != DEFL}}.
       join("")
   end
 
   def decode()
-    # 0 - prep. with placeholder ?
+    ## 0 - prep. with placeholder ?
     msg_ph = '?' * @msg.length
     r = fill(msg_ph)
-    # 1 - repl. placeholder
+
+    ## 1 - repl. placeholder
     lix, ix = 0, 0
-    0.upto(@rails - 1) do |jx|
+    each_rails do |jx|
       while ix < r[jx].length do
         if r[jx][ix] == '?'
           r[jx][ix] = @msg[lix]
@@ -42,35 +45,49 @@ class RailFenceCipher
       end
       ix = 0 # reset
     end
-    # 2 - construct decoded message
+
+    ## 2 - construct decoded message
     decoded = []
-    # assume all r have same lengths and read column by column
-    0.upto(r[0].length - 1) do |ix|
-      0.upto(@rails - 1) do |jx|
-        if r[jx][ix] != '.'
+
+    0.upto(r[0].length - 1) do |ix|  ## assume all r have same lengths (take r[0]) and read column by column
+      each_rails do |jx|
+        unless r[jx][ix] == '.'
           decoded.append(r[jx][ix])
-          break
+          break                      ## early exit
         end
       end
     end
-    #
-    return decoded.join('')
+
+    decoded.join('')
   end
 
   private
 
-  def fill(msg, defl=DEFL)
-    r = 0.upto(@rails - 1).inject([]) {|a, _| a << []}
-    kx, incr = 0, true
+  def each_rails
+    0.upto(@rails - 1) {|jx| yield jx if block_given?}
+  end
 
+  def enum_rails
+    (0...@rails)
+  end
+
+  def fill(msg, defl=DEFL)
+    ##
+    ## ex. "message to encode!", with @rails = 4
+    ##  rails | m e s s a g e t o e n c o d e
+    ##  -------------------------------------
+    ##  r[0]  | m . . . . . e . . . . . o . .
+    ##  r[1]  | . e . . . g . t . . . c . d .
+    ##  r[2]  | . . s . a . . . o . n . . . e
+    ##  r[3]  | . . . s . . . . . e . . . . .
+    ##
+    r = enum_rails.inject([]) {|a, _| a << []}
+
+    kx, incr = 0, true
     msg.each_char do |l|
-      0.upto(@rails - 1) do |jx|
-        if incr
-          r[jx].append(kx == jx ? l : defl)
-        else
-          ix = @rails - 2 - jx
-          r[ix].append(kx == ix ? l : defl)
-        end
+      each_rails do |jx|
+        ix = incr ? jx :  @rails - 2 - jx
+        r[ix].append(kx == ix ? l : defl)
       end
       kx, incr = incr ? incr_fn(kx, incr) : decr_fn(kx, incr)
     end
@@ -79,22 +96,22 @@ class RailFenceCipher
   end
 
   def incr_fn(kx, incr)
+    ##
+    ## Calc. increasing sequence 0, 1 .. @rails - 1, ends at @rails - 2
+    ##
     @rails == 1 && (return [0, incr])
     kx = (kx + 1) % @rails
-    if kx == 0
-      kx = @rails - 2
-      incr = false
-    end
+    kx == 0 && (kx, incr = @rails - 2, false)
     [kx, incr]
   end
 
   def decr_fn(kx, incr)
+    ##
+    ## Calc. decreasing sequence @rails - 2..0, ends at 1
+    ##
     @rails == 1 && (return [0, incr])
     kx -= 1
-    if kx == -1
-        kx = 1
-        incr = true
-    end
+    kx == -1 && (kx, incr = 1, true)
     [kx, incr]
   end
 
